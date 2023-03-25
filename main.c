d868fde0227edfaf3189a706951162ba5487d505
luis-dot-lopes <luisantonio20054@gmail.com>
2023-03-25 12:24:14 -0300

    Added function to dump data to ppm file

--- main.c
+++ main.c
@@ -26,15 +26,15 @@ typedef struct
 } pixel;
 
 pixel*
-decode_qoi(uint8_t* image_data, size_t image_len)
+decode_qoi(uint8_t* image_data, size_t image_len, size_t *image_width, size_t *image_height)
 {
   pixel seen_pixels[64] = { 0 };
   pixel prev_pixel = { .r = 0, .g = 0, .b = 0, .a = 255 };
-  size_t image_width = (image_data[4] << 24) + (image_data[5] << 16) + (image_data[6] << 8) + (image_data[7] << 0);
-  size_t image_height = (image_data[8] << 24) + (image_data[9] << 16) + (image_data[10] << 8) + (image_data[11] << 0);
-  printf("%c%c%c%c\n", image_data[0], image_data[1], image_data[2], image_data[3]);
-  printf("%d %d %lld\n", image_width, image_height, image_width * image_height);
-  pixel* decoded_pixels = malloc(sizeof(pixel) * image_width * image_height);
+  *image_width = (image_data[4] << 24) + (image_data[5] << 16) + (image_data[6] << 8) + (image_data[7] << 0);
+  *image_height = (image_data[8] << 24) + (image_data[9] << 16) + (image_data[10] << 8) + (image_data[11] << 0);
+  //printf("%c%c%c%c\n", image_data[0], image_data[1], image_data[2], image_data[3]);
+  //printf("%d %d %lld\n", image_width, image_height, image_width * image_height);
+  pixel* decoded_pixels = malloc(sizeof(pixel) * (*image_width) * (*image_height));
   image_data += sizeof(qoi_header);
   image_len -= sizeof(qoi_header);
   if(decoded_pixels == NULL) {
@@ -154,6 +154,21 @@ decode_qoi(uint8_t* image_data, size_t image_len)
   return decoded_pixels;
 }
 
+void 
+write_ppm_to_file(char *file_path, pixel *pixels, size_t image_width, size_t image_height)
+{
+  FILE* file = fopen(file_path, "wb");
+  fprintf(file, "P6\n%d %d 255\n", image_height, image_width);
+  for(size_t y = 0; y < image_height; ++y) {
+    for(size_t x = 0; x < image_width; ++x) {
+      fwrite(&pixels[y * image_width + x].r, sizeof(uint8_t), 1, file);
+      fwrite(&pixels[y * image_width + x].g, sizeof(uint8_t), 1, file);
+      fwrite(&pixels[y * image_width + x].b, sizeof(uint8_t), 1, file);
+    }
+  }
+  fclose(file);
+}
+
 uint8_t*
 load_file(char* file_path, size_t* file_len)
 {
@@ -186,7 +201,7 @@ main(int argc, char** argv)
     return 1;
   }
 
-  size_t image_len;
+  size_t image_len, image_width, image_height;
   uint8_t* image_data = load_file(argv[1], &image_len);
 
   printf("image_len: %d\n", image_len);
@@ -196,10 +211,12 @@ main(int argc, char** argv)
     return 1;
   }
 
-  pixel* pixels = decode_qoi(image_data, image_len);
+  pixel* pixels = decode_qoi(image_data, image_len, &image_width, &image_height);
   if (pixels == NULL) {
     return 1;
   }
+
+  write_ppm_to_file("out.ppm", pixels, image_width, image_height);
 
   free(pixels);
   free(image_data);
