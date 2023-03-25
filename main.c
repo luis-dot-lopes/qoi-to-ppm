0cd2accbd276525708277ed53e268f2f0c6beba6
luis-dot-lopes <luisantonio20054@gmail.com>
2023-03-25 11:45:48 -0300

    Fixed segfault

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+
+#include <fileapi.h>
+
+#endif
+
 typedef struct
 {
   char magic[4];
@@ -24,11 +30,17 @@ decode_qoi(uint8_t* image_data, size_t image_len)
 {
   pixel seen_pixels[64] = { 0 };
   pixel prev_pixel = { .r = 0, .g = 0, .b = 0, .a = 255 };
-  qoi_header header = *((qoi_header*)image_data);
+  size_t image_width = (image_data[4] << 24) + (image_data[5] << 16) + (image_data[6] << 8) + (image_data[7] << 0);
+  size_t image_height = (image_data[8] << 24) + (image_data[9] << 16) + (image_data[10] << 8) + (image_data[11] << 0);
+  printf("%c%c%c%c\n", image_data[0], image_data[1], image_data[2], image_data[3]);
+  printf("%d %d %lld\n", image_width, image_height, image_width * image_height);
+  pixel* decoded_pixels = malloc(sizeof(pixel) * image_width * image_height);
   image_data += sizeof(qoi_header);
   image_len -= sizeof(qoi_header);
-  uint32_t image_width = header.width, image_height = header.height;
-  pixel* decoded_pixels = malloc(sizeof(pixel) * image_width * image_height);
+  if(decoded_pixels == NULL) {
+      printf("Coundn't alloc memory\n");
+      return NULL;
+  }
   size_t decoded_count = 0;
   while (image_len > 8) {
     uint8_t byte = image_data[0];
@@ -46,7 +58,7 @@ decode_qoi(uint8_t* image_data, size_t image_len)
                                                  .a = prev_pixel.a };
         decoded_pixels[decoded_count] = cur_pixel;
         prev_pixel = cur_pixel; // May cause problems
-        seen_pixels[cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11] = cur_pixel;
+        seen_pixels[(cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11) % 64] = cur_pixel;
       } else if (byte & (0b11 << 6) == 0b10) {
         int dg = byte & ((1 << 6) - 1);
         image_data++;
@@ -64,9 +76,9 @@ decode_qoi(uint8_t* image_data, size_t image_len)
                                                  .b = prev_pixel.b + db,
                                                  .a = prev_pixel.a };
 
-        decoded_pixels[decoded_count] = cur_pixel
+        decoded_pixels[decoded_count] = cur_pixel;
         prev_pixel = cur_pixel; // May cause problems
-        seen_pixels[cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11] = cur_pixel;
+        seen_pixels[(cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11) % 64] = cur_pixel;
       } else {
         size_t run = byte & ((1 << 6) - 1);
         for(size_t i = 0; i < run; ++i) {
@@ -107,7 +119,7 @@ decode_qoi(uint8_t* image_data, size_t image_len)
         cur_pixel.a = byte;
         decoded_pixels[decoded_count] = cur_pixel;
         prev_pixel = cur_pixel;
-        seen_pixels[cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11] = cur_pixel;
+        seen_pixels[(cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11) % 64] = cur_pixel;
         decoded_count++;
       } else {
         pixel cur_pixel;
@@ -134,19 +146,27 @@ decode_qoi(uint8_t* image_data, size_t image_len)
 
         decoded_pixels[decoded_count] = cur_pixel;
         prev_pixel = cur_pixel;
-        seen_pixels[cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11] = cur_pixel;
+        seen_pixels[(cur_pixel.r * 3 + cur_pixel.g * 5 + cur_pixel.b * 7 + cur_pixel.a * 11) % 64] = cur_pixel;
         decoded_count++;
       }
     }
   }
+  return decoded_pixels;
 }
 
 uint8_t*
 load_file(char* file_path, size_t* file_len)
 {
   FILE* file = fopen(file_path, "rb");
+#ifdef _WIN32
   size_t size = GetFileSize(file, NULL);
-  uint8_t* file_content = malloc(size + 1);
+#endif
+#ifdef __linux__
+  fseek(file, 0, SEEK_END);
+  size_t size = ftell(file) + 1;
+  rewind(file);
+#endif
+  uint8_t* file_content = malloc(size);
   fread(file_content, size, 1, file);
   file_content[size] = 0;
   *file_len = size;
@@ -162,15 +182,17 @@ main(int argc, char** argv)
     printf("No input files\n");
     return 1;
   } else if (argc > 2) {
-    printf("Too many command line arguments");
+    printf("Too many command line arguments\n");
     return 1;
   }
 
   size_t image_len;
   uint8_t* image_data = load_file(argv[1], &image_len);
 
+  printf("image_len: %d\n", image_len);
+
   if (image_len < sizeof(qoi_header)) {
-    printf("Invalid image");
+    printf("Invalid image\n");
     return 1;
   }
 
@@ -180,6 +202,7 @@ main(int argc, char** argv)
   }
 
   free(pixels);
+  free(image_data);
 
   return 0;
 }
